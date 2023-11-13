f41a0a2147fc65af7e95984a84e0f32654dd7ad5
Shame You <shameyou13@gmail.com>
2023-11-13 13:16:44 +0800

    10e commit

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DetailsMoviesComponent } from '../details-movies/details-movies.component';
+import { DetailsReviewsComponent } from '../details-reviews/details-reviews.component';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DetailsMoviesComponent, DetailsReviewsComponent],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
